3888993573359305be5adc670068a6c6f002baf2
Shlomo Morosow <shlomorosow@gmail.com>
2020-12-08 15:25:09 +0200

    test: add date-fns to jest config compiler blacklist

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     preset: 'jest-preset-angular',
     setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
     transformIgnorePatterns: [
-        "node_modules/(?!@ngrx)"
+        "node_modules/(?!@ngrx|date-fns)"
     ],
     transform: {
         '^.+\\.(ts|js|html)$': 'ts-jest',
